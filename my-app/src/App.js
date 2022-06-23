e45b0b292befa5ec71e207bf065a30cb0afc2f8c
Cata Olivar <cataolivar@gmail.com>
2022-06-23 01:03:17 -0300

    first

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
 import './index.css';
 import Header from "./components/Header/Header"
 import NavBar from "./components/Navbar/NavBar";
-import CartWidget from "./components/CartWidget/CartWidget"
 import React from 'react'
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
@@ -12,7 +11,6 @@ const App = () => {
 
 <Header/>
 <NavBar/>
-<CartWidget/>
 <ItemListContainer greeting= "Bienvenidos a Kalaka"/>
 
     </div>
